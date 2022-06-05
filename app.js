6e03cfab8ccda21014e0decddcb83773e6491f6c
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2022-06-05 14:53:35 +0200

    Initializing project

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const express = require('express');
-const graphql = require('graphql');
+const express = require("express");
+const { graphql, buildSchema } = require("graphql");
 const app = express();
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The rootValue provides a resolver function for each API endpoint
+var rootValue = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql({
+    schema,
+    source: '{ hello }',
+    rootValue
+  }).then((response) => {
+    console.log(response);
+  });
